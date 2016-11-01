36edccf7ecb27650d726108e873d96baa0b423d0
crtenbarge <crtenbarge@gmail.com>
2016-10-31 21:51:04 -0500

    Update bargeFolio.py

--- bargeFolio.py
+++ bargeFolio.py
@@ -1,37 +1,43 @@
-import subprocess
-import csv
 import os
+import csv
+import subprocess
 
-def makeFolder (ou): 
-	proc=subprocess.Popen('~/gam/gam.py user portfolio@lssd.org update drivefile drivefilename "Prototype Folder2" mimetype gfolder', shell=True, stdout=subprocess.PIPE, )
+teacher='teacher@school.org'
+portfolioAccount='portfolio@school.org'
+domain='school.org'
+
+parentFolder = '0123456789abcdefghijklmnopqr' #the id of the parent folder
+ou = '/staff/or/student/org' #the OU where your students are
+folderPrefix = 'Project_'  #no spaces!!
+
+##Function that makes the folders
+def makeFolder (org, parent, folderName, email): 
+	proc=subprocess.Popen('~/gam/gam.py user '+portfolioAccount+' update drivefile drivefilename '+folderName+' mimetype gfolder parentid '+parent, shell=True, stdout=subprocess.PIPE, )
 	output=proc.communicate()[0]
+	print output
 	if "No" in output:
 		print "Creating a Folder"
-		proc1=subprocess.Popen('python ~/gam/gam.py user portfolio@lssd.org add drivefile drivefilename "Prototype Folder2" mimetype gfolder', shell=True, stdout=subprocess.PIPE, )
+		proc1=subprocess.Popen('python ~/gam/gam.py user '+portfolioAccount+' add drivefile drivefilename '+folderName+' mimetype gfolder parentid '+parent, shell=True, stdout=subprocess.PIPE, )
 		output=proc1.communicate()[0].split()
 		key = max(output, key=len)
-		student='s1@lssd.org'
-		bashCommand1 = "python ~/gam/gam.py user portfolio@lssd.org add drivefileacl %s user %s role writer" % (key, teacher)
-		bashCommand2 = "python ~/gam/gam.py user portfolio@lssd.org add drivefileacl %s user %s role reader" % (key, student)
+		bashCommand1 = "python ~/gam/gam.py user %s add drivefileacl %s user %s role writer" % (portfolioAccount, key, teacher)
+		bashCommand2 = "python ~/gam/gam.py user %s add drivefileacl %s user %s role reader" % (portfolioAccount, key, email)
 		os.system(bashCommand1)
 		os.system(bashCommand2)
 	else:
 		print "Folder already created"
 
+##Make a list of users
+getUsers = 'python ~/gam/gam.py print users ou suspended gal > portfolioData.csv'
+os.system(getUsers)
 
-bashCommandQ = 'alias gam="python ~/gam/gam.py"'
-os.system(bashCommandQ)
-bashCommandX = 'python ~/gam/gam.py print users suspended > userEmails.csv'
-os.system(bashCommandX)
-
-#open the csv
-with open('userEmails.csv', 'rb') as f:
+#Loop through the list of users
+with open('portfolioData.csv', 'rb') as f:
     reader = csv.reader(f)
     print 'Lets get this party started!'
     for row in reader:
-		email = row[0]
-		if '@lssd.org' in email and row[1] == "False":
-			bashCommandZ = 'python ~/gam/gam.py user %s update photo http://cdn.lssd.org/staff/%s.jpg' % (row[0], row[0])
-#			print bashCommandZ
-			os.system(bashCommandZ)			
-    print 'Sync complete'
+		folderName = folderPrefix+email
+		if domain in email and suspended == 'False' and gal == 'True':
+			if org == ou                                     ##These two lines can be repeated for as many OUs
+				makeFolder(tmp, art1, folderName, email)     ##as you can manage. 
+	print 'Whew!'
